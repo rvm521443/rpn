9a5673b7219626007e7c3146ba2594943ae79d63
rvm521443 <rvm521443@gmail.com>
2023-02-02 21:49:20 +0300

    another test

--- src/test.py
+++ src/test.py
@@ -9,5 +9,9 @@ class TestCalcOp1(unittest.TestCase):
         self.calc.op1(lambda _: 0)
         self.assertEqual([1.0, 2.0, 3.0, 0.0], self.calc.st)
 
+    def testSquare(self):
+        self.calc.op1(lambda x: x * x)
+        self.assertEqual([1.0, 2.0, 3.0, 16.0], self.calc.st)
+
 if __name__ == '__main__':
     unittest.main()
