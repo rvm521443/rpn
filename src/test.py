a4837e74e8fe3a9ab01772ae4659f849c7b1a186
rvm521443 <rvm521443@gmail.com>
2023-02-03 23:25:46 +0300

    removed duplicate test

--- src/test.py
+++ src/test.py
@@ -7,12 +7,7 @@ class TestCalc(unittest.TestCase):
         self.calc = rpn.Calc([float(x) for x in range(1, 5)])
 
 
-
 class TestCalcOp1(TestCalc):
-    def testClear(self):
-        self.calc.op1(lambda _: 0)
-        self.assertEqual([1.0, 2.0, 3.0, 0.0], self.calc.st)
-
     def testSquare(self):
         self.calc.op1(lambda x: x * x)
         self.assertEqual([1.0, 2.0, 3.0, 16.0], self.calc.st)
